1999643f984969eed185f2209b093b64d1b24c7e
fwarmuth <felix.warmuth@mailbox.org>
2019-02-28 19:34:24 +0100

    Refactor parser

--- parser.py
+++ parser.py
@@ -1,99 +1,47 @@
-from collections import deque
+from picture import Picture
 
-class City(object):
-    def __init__(self, size, vehicles, num_rides, bonus, steps, rides):
-        self.size = size
-        self.vehicles = vehicles
-        self.num_rides = num_rides
-        self.bonus = bonus
-        self.steps = steps
-        self.rides = rides
+class IO:
+    """Handles file IO"""
 
-    def __repr__(self):
-        return "City: \n \
-Size: {} \n \
-Vehicles: {} \n \
-Number of rides: {} \n \
-Bonus: {} \n \
-Steps: {} \n \
-Rides: {} \n".format(self.size, self.vehicles, self.num_rides, self.bonus, self.steps, self.rides)
-
-class Ride(object):
-    def __init__(self, id, start_from, end_at, earliest_start, latest_finish):
-        self.id = id
-        self.start_from = start_from
-        self.end_at = end_at
-        self.earliest_start = earliest_start
-        self.latest_finish = latest_finish
-
-    def __repr__(self):
-        return "Ride {}: \n \
-From: {} \n \
-To: {} \n \
-Earliest start: {} \n \
-Latest_finish: {} \n".format(self.id, self.start_from, self.end_at, self.earliest_start, self.latest_finish)
-
-class Vehicle(object):
     def __init__(self):
-        self.current_position = (0, 0)
-        self.current_time = 0
-        self.ride_queue = deque()
+        """Constructor for io"""
 
-    def __repr__(self):
-        return "Vehicle: \n \
-Position: {} \n \
-Queued Rides: {} \n".format(self.current_position, self.ride_queue)
+    def _read_file_line_by_line(self, filepath):
+        """
+        Read file line by line
+        :param filepath:
+        :return: List of all lines
+        """
+        with open(filepath) as f:
+            content = f.readlines()
 
+        # you may also want to remove whitespace characters like `\n` at the end of each line
+        content = [x.strip() for x in content]
 
-def load_solution(solution_path, city):
-    lines = get_lines_from_file(solution_path)
-    tours = [[city.rides[int(tour_id)] for tour_id in line.split()[1:]] for line in lines]
+        return content
 
-    if len(tours) != len(city.vehicles):
-        raise Exception("Length of tours %s does not match number of vehicles %s." % (len(tours), len(city.vehicles)))
+    def load_data(self, filepath):
+        """
+        Loads data from file and return it in a structured manner
+        :param filepath:
+        :return:
+        """
+        raw = self._read_file_line_by_line(filepath)
+        data = {}
+        # read number of pictures
+        data['N'] = int(raw[0])
+        #del 1st row
+        raw = raw[1:]
+        pictures = []
+        for i, picture in enumerate(raw[1:]):
+            # for all following lines
+            row = list(raw[i].split())
+            isHorizontal = bool(row[0] == 'H')
+            tag_list = []
+            for tag in row[2:]:
+                tag_list.append(tag)
+            # create picture and append it
+            pictures.append(Picture(i, isHorizontal, tag_list))
 
-    for i, vehicle in enumerate(city.vehicles):
-        vehicle.ride_queue = deque(tours[i])
-
-    return city
-
-def save_solution(solution_path, city):
-    output = [str(len(vehicle.ride_queue)) + " " + " ".join(map(lambda ride: str(ride.id), vehicle.ride_queue)) for vehicle in city.vehicles]
-
-    with open(solution_path, 'w') as file:
-        file.write("\n".join(output))
-
-
-def get_lines_from_file(path):
-    with open(path, 'r') as file:
-        content = file.read()
-
-    return [line for line in content.split("\n") if len(line) > 0]
-
-
-def interpret_lines(lines):
-    # get attributes from first line
-    first_line_numbers = [int(number_str) for number_str in lines[0].split()]
-    city_size = (first_line_numbers[0], first_line_numbers[1])
-    vehicles = [Vehicle() for i in range(first_line_numbers[2])]
-    num_rides = first_line_numbers[3]
-    bonus = first_line_numbers[4]
-    steps = first_line_numbers[5]
-
-    # get rides from rest of lines
-    rides = []
-    for ride_id, line in enumerate(lines[1:]):
-        line_numbers = [int(number_str) for number_str in line.split()]
-        start_from = (line_numbers[0], line_numbers[1])
-        end_at = (line_numbers[2], line_numbers[3])
-        earliest_start = line_numbers[4]
-        latest_finish = line_numbers[5]
-        rides.append(Ride(ride_id, start_from, end_at, earliest_start, latest_finish))
-
-    return City(city_size, vehicles, num_rides, bonus, steps, rides)
-
-
-def load_problem(path):
-    lines = get_lines_from_file(path)
-    city = interpret_lines(lines)
-    return city
+        data['pictures'] = pictures
+        return data
