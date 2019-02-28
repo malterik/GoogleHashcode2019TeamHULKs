7222f3afac611dc19b2358cfa523f86b43a80063
Erik Schröder <schroedererik@mailbox.org>
2019-02-28 20:43:19 +0100

    Scoring should work

--- scoring.py
+++ scoring.py
@@ -6,6 +6,8 @@ import argparse
 from parser import load_data, create_solution_file
 from slideshow import Slideshow
 from slide import Slide
+from solution import score_solution
+from picture import Picture
 #  from simulate import score_solution
 
 
@@ -20,16 +22,16 @@ def main():
     solution_path = args.solution
     
     data = load_data(problem_path)
-    print(data)
     s = Slideshow()
     slide1 = Slide()
-    slide1.picture1 = 1
+    slide1.picture1 = 0
     slide2 = Slide()
-    slide2.picture1 = 0
+    slide2.picture1 = 1
     slide2.picture2 = 2
     s.add_slide(slide1)
     s.add_slide(slide2)
-    solution = create_solution_file(s, solution_path)
+    #  solution = create_solution_file(s, solution_path)
+    score = score_solution(s, data)
 
 if __name__ == '__main__':
     main()
--- solution.py
+++ solution.py
@@ -1,7 +1,33 @@
 from slideshow import Slideshow
 from slide import Slide
-def score_solution(slideshow):
-    total_points = 0
-    t = 0
-    return total_points
+import numpy as np
+def score_solution(slideshow, input_data):
+    print(input_data)
+    total_score = 0
+    for i,slide in enumerate(slideshow.slides):
+        p1_tags=[]
+        p2_tags=[]
+        if slide.picture2 == None: # horizontal image
+            p1_tags = input_data[slide.picture1].tags
+        else:
+            p1_tags = list(set(input_data[slide.picture1].tags + input_data[slide.picture2].tags))
+
+        if i + 1 < len(slideshow.slides):
+            slide2 = slideshow.slides[i + 1]
+            if slide2.picture2 == None: # horizontal image
+                p2_tags = input_data[slide2.picture1].tags
+            else:
+                p2_tags = list(set(input_data[slide2.picture1].tags + input_data[slide2.picture2].tags))
+        else:
+            continue
+        print("p1_tags")
+        print(p1_tags)
+        print("p2_tags")
+        print(p2_tags)
+        common_tags = len(list(set(p1_tags).intersection(p2_tags)))
+        tags_in_1 = len(np.setdiff1d(p1_tags, p2_tags))
+        tags_in_2 = len(np.setdiff1d(p2_tags, p1_tags))
+        tempscore = min(common_tags, tags_in_1, tags_in_2)
+
+    return total_score
 
